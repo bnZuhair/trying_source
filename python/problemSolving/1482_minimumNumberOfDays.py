22d2473ca83d8b2307f33b3e78d26ac975c2adf4
bnZuhair <relsalehzohair@gmail.com>
2024-06-30 06:12:26 +0300

    trying to solve the problem for the first time

--- python/problemSolving/1482_minimumNumberOfDays.py
+++ python/problemSolving/1482_minimumNumberOfDays.py
@@ -1,15 +1,37 @@
-        """
-        :type bloomDay: List[int]
-        :type m: int
-        :type k: int
-        :rtype: int
-        """
+import bisect
 class Solution(object):
     def minDays(self, bloomDay, m, k):
-        totalflowersneeded = m * k
-        if( totalflowersneeded < bloomDay):
+        minFlowerNeeded = m * k
+        if( len(bloomDay) < minFlowerNeeded ):
             return -1
         else:
-          
+            if( k == 1):
+                bloomDay = sorted(bloomDay)
+                return bloomDay[m - 1]
+            else:
+                remaningFlowers = len(bloomDay) - minFlowerNeeded  
 
-
+                listOfBouquets = []
+                usedIndices = []
+                adjFlowers = 0
+                maxDay = 0
+                
+                for i in range(0, remaningFlowers):
+                    lastIndex = len(bloomDay) - 1
+                    maxDay = max(bloomDay[lastIndex:lastIndex - k:-1])
+                    maxDayIndex = bloomDay.index(maxDay)
+                    if( maxDayIndex not in usedIndices):
+                        usedIndices.append( maxDayIndex )
+                        bisect.insort(listOfBouquets, maxDay)
+                    
+                for i in range(minFlowerNeeded, -1, -1):
+                    adjFlowers += 1
+                    if(maxDay < bloomDay[i]):
+                        maxDay = bloomDay[i]
+                    if(adjFlowers == k ):
+                        bisect.insort(listOfBouquets, maxDay)
+                        maxDay = 0
+                        adjFlowers = 0
+                if(len(listOfBouquets) < m):
+                    return -1
+                return listOfBouquets[m - 1]
