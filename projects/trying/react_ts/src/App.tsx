d5165d3eef8eaab30c8e2ef172d9bf993db5b3a0
bnzuhair <relsalehzohair@gmail.com>
2025-12-22 14:49:58 +0300

    feat: componants skeleton

--- projects/trying/react_ts/src/App.tsx
+++ projects/trying/react_ts/src/App.tsx
@@ -1,2 +1,31 @@
 export default function App() {
+   return (
+      <div className="flex justify-center items-center">
+
+         <Clock min={"00"} sec={"10"} />
+
+      </div>
+   );
+}
+
+interface ClockProps {
+   min: string;
+   sec: string;
+}
+
+function Clock({ min, sec }: ClockProps) {
+   return (
+      <div className="">
+         <Conter value={min} />
+         <Conter value={sec} />
+      </div>
+   )
+}
+
+function Conter({ value }: { value: string }) {
+   return (
+      <input
+         className="text-red-400 orbitron text-xl"
+         value={value} />
+   );
 }
