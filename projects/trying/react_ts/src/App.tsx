47adaf4a8d79a6c7395ffa710bbf09309e55f33c
bnzuhair <relsalehzohair@gmail.com>
2025-12-27 14:52:45 +0300

    feat: adding the buttons and the counters

--- projects/trying/react_ts/src/App.tsx
+++ projects/trying/react_ts/src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
    return (
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center h-screen">
 
          <Clock min={"00"} sec={"10"} />
 
@@ -13,16 +13,32 @@ interface ClockProps {
    sec: string;
 }
 
-function Clock({ min, sec }: ClockProps) {
+function Clock({ min = "00", sec = "00" }: ClockProps) {
    return (
-      <div className="">
-         <Conter value={min} />
-         <Conter value={sec} />
+      <div className="flex-col">
+         <div>
+            <Conter key='1' value={min} />
+            <Conter key='0' value={sec} />
+         </div>
+         <div className="flex justify-around">
+            <Button text="stop" />
+            <Button text="start" />
+         </div>
       </div>
-   )
+   );
+}
+interface ButtonProps {
+   text: string,
+   handler: () => void
+}
+function Button({ text, handler = () => { } }: ButtonProps) {
+   return (
+      <button className="text-green-200" onClick={handler}>{text}</button>
+   );
 }
 
 function Conter({ value }: { value: string }) {
+
    return (
       <input
          className="text-red-400 orbitron text-xl"
