e8701d0ecd36e1ca8dc9da94eb496d056bdddb5c
bnzuhair <relsalehzohair@gmail.com>
2025-12-28 12:58:24 +0300

    feat: counting down timer that is connected with the counter

--- projects/trying/react_ts/src/App.tsx
+++ projects/trying/react_ts/src/App.tsx
@@ -8,19 +8,33 @@ export default function App() {
 }
 
 
+// const delay = (time: number) => { return new Promise(resolve => { setTimeout(resolve, time) }) };
+//       await delay(1000);
 function Clock() {
    const [min, setMin] = useState(0);
    const [sec, setSec] = useState(0);
+
+   async function startTimer() {
+      //maybe
+      while (sec > 0) {
+         await new Promise(resolve => { setTimeout(resolve, 1000) });
+         setSec(sec => sec - 1);
+      }
+   }
+   function restHandler() {
+      setMin(0);
+      setSec(0);
+   }
    return (
       <div className="flex flex-col">
          <div className="text-red-400 orbitron">
-            <Counter key='0' value={min} />
+            <Counter key='0' value={min} onChange={setMin} />
             <span className="text-9xl">:</span>
-            <Counter key='1' value={sec} />
+            <Counter key='1' value={sec} onChange={setSec} />
          </div>
          <div className="flex justify-around">
-            <Button text="rest" />
-            <Button text="start" />
+            <Button key='0' text="rest" handler={restHandler} />
+            <Button key='1' text="start" handler={startTimer} />
          </div>
       </div>
    );
@@ -35,9 +49,7 @@ function Button({ text, handler = () => { } }: ButtonProps) {
    );
 }
 
-function Counter({ value }: { value: number }) {
-
-   const [num, setNum] = useState(value);
+function Counter({ value, onChange }: { value: number, onChange: (numb: number) => void }) {
 
    function changeHandler(e: ChangeEvent<HTMLInputElement>) {
       const rawValue = e.target.value;
@@ -47,14 +59,14 @@ function Counter({ value }: { value: number }) {
       } else if (value < 0) {
          value = 0;
       }
-      setNum(value);
+      onChange(value);
    }
    const noButtonDefaults = "focus:outline-0 box-border appearance-none [&::-webkit-inner-spin-button,&::-webkit-outer-spin-button]:appearance-none";
 
    return (
       <input type="number" min={0} max={59}
          className={`text-9xl w-[2ch] ${noButtonDefaults}`}
-         value={num.toString().padStart(2, '0')}
+         value={value.toString().padStart(2, '0')}
          onChange={changeHandler} />
    );
 }
